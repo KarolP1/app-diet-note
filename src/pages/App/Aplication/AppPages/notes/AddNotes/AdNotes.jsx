8e3e6fd00b82a1439f89f28c172b134db0f595e6
KarolP1 <52258025+KarolP1@users.noreply.github.com>
2020-11-20 00:42:42 +0100

    1

--- src/pages/App/Aplication/AppPages/notes/AddNotes/AdNotes.jsx
+++ src/pages/App/Aplication/AppPages/notes/AddNotes/AdNotes.jsx
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { MainContainer } from "./AdNotes.styled";
 import { Editor } from "@tinymce/tinymce-react";
 
 const AdNotes = (props) => {
-	const handleEditorChange = (content, editor) => {
-		console.log("Content was updated:", content);
-	};
+	const [editorValue, setEditorValue] = useState("");
+
 	return (
 		<MainContainer visible={props.visible}>
 			<form>
@@ -17,10 +16,9 @@ const AdNotes = (props) => {
 			</form>
 			<Editor
 				apiKey="mudahddk8akmnpsm2yod7myf1zx4p2h7tyybwnkbkj5d5325"
-				initialValue="<p>This is the initial content of the editor</p>"
 				init={{
 					height: 500,
-					menubar: false,
+					menubar: true,
 					plugins: [
 						"advlist autolink lists link image charmap print preview anchor",
 						"searchreplace visualblocks code fullscreen",
@@ -29,8 +27,9 @@ const AdNotes = (props) => {
 					toolbar:
 						"undo redo | formatselect | bold italic backcolor | table | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
 				}}
-				onEditorChange={handleEditorChange}
+				onEditorChange={(e, editor) => setEditorValue(editor.getContent())}
 			/>
+			{editorValue}
 		</MainContainer>
 	);
 };
