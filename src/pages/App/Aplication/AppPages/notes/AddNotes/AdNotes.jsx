8842fcf8b5053ea6adc1d88f0643489b3f343c59
KarolP1 <52258025+KarolP1@users.noreply.github.com>
2020-11-19 23:40:56 +0100

    tiny better

--- src/pages/App/Aplication/AppPages/notes/AddNotes/AdNotes.jsx
+++ src/pages/App/Aplication/AppPages/notes/AddNotes/AdNotes.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { MainContainer } from "./AdNotes.styled";
-
+import { Editor } from "@tinymce/tinymce-react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 import "suneditor/dist/css/suneditor.min.css";
 
 const AdNotes = (props) => {
+	const handleEditorChange = (content, editor) => {
+		console.log("Content was updated:", content);
+	};
 	return (
 		<MainContainer visible={props.visible}>
 			<form>
@@ -16,23 +19,21 @@ const AdNotes = (props) => {
 				</label>
 				<input type="submit" value="Submit" />
 			</form>
-			<CKEditor
-				editor={ClassicEditor}
-				data="<p>Hello from CKEditor 5!</p>"
-				onReady={(editor) => {
-					// You can store the "editor" and use when it is needed.
-					console.log("Editor is ready to use!", editor);
+			<Editor
+				apiKey="mudahddk8akmnpsm2yod7myf1zx4p2h7tyybwnkbkj5d5325"
+				initialValue="<p>This is the initial content of the editor</p>"
+				init={{
+					height: 500,
+					menubar: false,
+					plugins: [
+						"advlist autolink lists link image charmap print preview anchor",
+						"searchreplace visualblocks code fullscreen",
+						"insertdatetime media table paste code help wordcount",
+					],
+					toolbar:
+						"undo redo | formatselect | bold italic backcolor | table | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
 				}}
-				onChange={(event, editor) => {
-					const data = editor.getData();
-					console.log({ event, editor, data });
-				}}
-				onBlur={(event, editor) => {
-					console.log("Blur.", editor);
-				}}
-				onFocus={(event, editor) => {
-					console.log("Focus.", editor);
-				}}
+				onEditorChange={handleEditorChange}
 			/>
 		</MainContainer>
 	);
